ffc22c5dc3213d08292263abf2e18e71849ef5bc
Chris <christopher@christophervigliotti.com>
2023-04-25 16:53:59 -0400

    wip

--- 02_basic_ruby/basic_enumerable_methods/odin.rb
+++ 02_basic_ruby/basic_enumerable_methods/odin.rb
@@ -35,8 +35,6 @@ my_array.each do |num|
     puts "The new number is #{num}."
 end
 
-=end
-
 # each with key and value as defined by separate block variables
 my_hash = { "one" => 1, "two" => 2 }
 my_hash.each { |key, value| puts "#{key} is #{value}" }
@@ -44,5 +42,85 @@ my_hash.each { |key, value| puts "#{key} is #{value}" }
 # each with the key and value defined together in one block variable
 my_hash.each { |pair| puts "the pair is #{pair}" }  
 # my_hash.each { |pair| puts pair.methods.sort }  
-
 # resume at "You may have noticed in the above code examples that #each returns the original array or hash regardless of what happens inside the code block"
+
+# iterates over but dos not alter the elements in an array
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+friends.each { |friend| friend.upcase }
+# print friends
+
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+shouting_at_friends = []
+friends.each { |friend| shouting_at_friends.push(friend.upcase) }
+# print shouting_at_friends
+
+# map
+# Map returns a new array with the results
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+friends_mapped = friends.map { |friend| friend.upcase }
+p friends_mapped
+
+my_order = ['medium Big Mac', 'medium fries', 'medium milkshake']
+my_order_mapped = my_order.map { |item| item.gsub('medium', 'extra large') }
+p my_order_mapped
+
+salaries = [1200, 1500, 1100, 1800]
+salaries_mapped = salaries.map { |salary| salary - 700 }
+p salaries_mapped
+
+=end
+
+
+# select
+
+    friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+    invited_list = []
+    friends.each do |friend|
+    if friend != 'Brian'
+        invited_list.push(friend)
+    end
+    end
+    #p invited_list
+
+    friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+    invited_list = friends.select { |friend| friend != 'Brian' }
+    #p invited_list
+
+    responses = { 'Sharon' => 'yes', 'Leo' => 'no', 'Leila' => 'no', 'Arun' => 'yes' }
+    yesses = responses.select { |person, response| response == 'yes'}
+    #p yesses
+
+# reduce
+
+    my_numbers = [5, 6, 7, 8]
+    sum = 0
+    #p my_numbers.each { |number| sum += number }
+    #p sum
+
+    my_numbers = [5, 6, 7, 8]
+    sum = my_numbers.reduce { |sum, number| sum + number }
+    #p sum
+
+    my_numbers = [5, 6, 7, 8]
+    #p my_numbers.reduce(1000) { |sum, number| sum + number }
+
+    votes = ["Bob's Dirty Burger Shack", "St. Mark's Bistro", "Bob's Dirty Burger Shack"]
+    votes_counted = votes.reduce(Hash.new(0)) do |result, vote|
+    result[vote] += 1
+    result
+    end
+    #p votes_counted
+
+hashy = Hash.new('default value')
+#p hashy["wat"]
+
+# bang methods (they modify things!)
+
+    friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+    friends.map { |friend| friend.upcase }
+    p friends
+
+    friends.map! { |friend| friend.upcase }
+    p friends
+
+# return values of enumerables
