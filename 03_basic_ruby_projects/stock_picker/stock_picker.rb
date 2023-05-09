5bc69178ff755940e1e020e1a5acbfe5b8d5f11f
Chris <christopher@christophervigliotti.com>
2023-05-09 14:38:33 -0400

    work complete

--- 03_basic_ruby_projects/stock_picker/stock_picker.rb
+++ 03_basic_ruby_projects/stock_picker/stock_picker.rb
@@ -1,9 +1,8 @@
 def stock_picker(price_each_day_array)
-    return_var = 'do not buy'
-    min_price_and_day = price_each_day_array.each_with_index.min
-    buy_on_day = min_price_and_day[1]
-    sell_on_day =  price_each_day_array.slice(min_price_and_day[1],price_each_day_array.length()).each_with_index.max[1] + buy_on_day
-    (buy_on_day < sell_on_day) ? [buy_on_day,sell_on_day] : return_var
+    min_price_and_day = price_each_day_array.each_with_index.min # get an arry with [lowest number,array position of lowest number]
+    buy_on_day = min_price_and_day[1] # the position of the lowest number
+    sell_on_day =  price_each_day_array.slice(min_price_and_day[1],price_each_day_array.length()).each_with_index.max[1] + buy_on_day # the position of the highest number that appears after the lowest number
+    (buy_on_day < sell_on_day) ? [buy_on_day,sell_on_day] : "do not buy" # return an array of [lowest,highest] unless no high number that meets the criteria is found
 end
-p "APPL: #{stock_picker([209,210,195,235])}"
-p "F: #{stock_picker([17,3,6,9,15,8,6,1])}"
+p "APPL: #{stock_picker([209,210,195,235])}" # should return an array
+p "F: #{stock_picker([17,3,6,9,15,8,6,1])}" # should return "do not buy"
