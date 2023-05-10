f01c8f84c3b0e81b7f3ec34e4295354dba328414
Chris <christopher@christophervigliotti.com>
2023-05-10 13:45:10 -0400

    wip

--- 03_basic_ruby_projects/cipher/cipher_pr_03.rb
+++ 03_basic_ruby_projects/cipher/cipher_pr_03.rb
@@ -1,9 +1,15 @@
+#4. there is also a method to take an array of two arrays and turn it into a hash.
+#[[a,b,c],[d,e,f]].... -> {a => d, ...probably a `.transpose.to_h`
+
+#5. the hash .invert method https://ruby-doc.org/core-2.7.0/Hash.html#method-i-invert
+#will probably also be useful 
 
 def caesar_encode(string,key,encode_or_decode = 'encode') 
     encoded_string = '' 
     alphabet_array = ('a'..'z').to_a 
     rotate_to_position = (encode_or_decode == 'encode') ? key : -key
     key_array = alphabet_array.rotate(rotate_to_position)
+    puts Hash[alphabet_array.zip(key_array)] # 4. there is also a method to take an array of two arrays and turn it into a hash.
     string.each_char.with_index(1){|char, index| # 1. strings have some nifty helper methods `.each_char.with_index `
         lowercase_char = char.downcase 
         char_is_letter = char.match(/^[[:alpha:]]$/) != nil
