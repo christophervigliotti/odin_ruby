89cea6462bd1350f8b9e79d7624d612bea048745
Chris <christopher@christophervigliotti.com>
2023-05-10 14:34:43 -0400

    wip

--- 03_basic_ruby_projects/cipher/cipher_pr_04.rb
+++ 03_basic_ruby_projects/cipher/cipher_pr_04.rb
@@ -5,9 +5,7 @@ def caesar_encode(string,key,encode_or_decode = 'encode')
     rotate_to_position = (encode_or_decode == 'encode') ? key : -key
     key_array = alphabet_array.rotate(rotate_to_position)
     map_hash = Hash[alphabet_array.zip(key_array)]
-    string.each_char{|character| 
-        encoded_string += map_hash[character] || character
-    }
+    string.each_char{|character|encoded_string += map_hash[character] || character}
     encoded_string # return variable
 end
 p caesar_encode('Et tu, Bluto?',5)
