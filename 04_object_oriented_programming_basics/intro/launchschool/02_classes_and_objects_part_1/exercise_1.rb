f5f6d8f751a55297e4e309e3809f75caa257be5e
Chris <christopher@christophervigliotti.com>
2023-05-24 15:58:13 -0400

    lauchschool wip

---
+++ 04_object_oriented_programming_basics/intro/launchschool/02_classes_and_objects_part_1/exercise_1.rb
@@ -0,0 +1,34 @@
+class MyCar
+    attr_accessor :color, :model, :speed
+    attr_reader :year
+  
+    def initialize(year, color, model)
+      @year = year
+      @color = color
+      @model = model
+      @speed = 0
+    end
+  
+    def speed_up
+        puts "speeding up from #{@speed} to #{@speed + 5}"
+        @speed = @speed + 5
+    end
+
+    def brake
+        @speed = 0
+        puts "slamming on the brakes"
+    end
+
+    def shut_off
+        self.brake
+        puts "shutting off"
+    end
+
+end
+
+pacer = MyCar.new(year = 1976, color = "Light Blue", Model = "Pacer")
+puts pacer.year
+pacer.speed_up
+pacer.speed_up
+pacer.speed_up
+pacer.brake
