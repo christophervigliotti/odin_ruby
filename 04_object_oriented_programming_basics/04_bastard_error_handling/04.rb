c0071fcdabeeaf60194c703057480985ede58890
Chris <christopher@christophervigliotti.com>
2024-01-25 16:40:20 -0500

    bastard error handling DONE, cleanup DONE

---
+++ 04_object_oriented_programming_basics/04_bastard_error_handling/04.rb
@@ -0,0 +1,17 @@
+=begin
+    while 1
+        puts "Enter a number>>"
+        num = Kernel.gets.match(/\d+/)[0]
+        puts "#{num} + 1 is: #{num.to_i+1}"
+    end
+=end
+while 1
+    puts "Enter a number>>"
+    begin
+      num = Kernel.gets.match(/\d+/)[0]
+    rescue
+      puts "Erroneous input! Try again..."
+    else  
+      puts "#{num} + 1 is: #{num.to_i+1}"
+    end  
+ end
---
+++ 04_object_oriented_programming_basics/04_bastard_error_handling/07.rb
@@ -0,0 +1,32 @@
+require 'open-uri'
+remote_base_url = "https://en.wikipedia.org/wiki"
+
+[1900, 1910, 'xj3490', 2000].each do |yr|
+ 
+    retries = 3
+    
+    begin
+        url = "#{remote_base_url}/#{yr}"
+        puts "Getting page #{url}"
+        rpage = open(url)
+    rescue StandardError=>e
+        puts "\tError: #{e}"
+        #if !e.message.to_s.include? "Can't get"
+            if retries > 0
+                #puts "\tTrying #{retries} more times"
+                puts 'e.class: ' + e.class.to_s
+                puts 'e.message: ' + e.message.to_s
+                retries -= 1
+                #sleep 1
+                retry
+            else
+                puts "\t\tCan't get #{yr}, so moving on"
+            end    
+        #end
+    else
+        puts "\tGot page for #{yr}"
+    ensure   
+        #puts "Ensure branch; sleeping"
+        #sleep 1
+    end
+end
