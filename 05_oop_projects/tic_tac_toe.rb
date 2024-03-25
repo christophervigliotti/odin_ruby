e71fee1577ce16282ddb2bf7a3bb717c7df9ff7a
Chris <christopher@christophervigliotti.com>
2024-03-25 13:42:48 -0400

    added method "draw_board"

--- 05_oop_projects/tic_tac_toe.rb
+++ 05_oop_projects/tic_tac_toe.rb
@@ -10,18 +10,32 @@ class TicTacToe
     [1, 5, 9],
     [3, 5, 7]
   ]
+  EMPTY_BOARD = "1 | 2 | 3
+  --------
+  4 | 5 | 6
+  --------
+  7 | 8 | 9
+  "
 
   def broadcast(message)
     @message_history.append(message)
     puts message
   end
 
+  def change_player
+    @whose_turn = @whose_turn == 1 ? 0 : 1
+  end
+
   def check_for_win
     array.map! do |item|
-      if(item - @moves[@whose_turn]).empty?
-        end_game
-      end
+      end_game unless (item - @moves[@whose_turn]).empty?
     end
+  end
+
+  def draw_board
+    @moves[0].each { |x| @board.sub! x.to_s, 'X' }
+    @moves[1].each { |x| @board.sub! x.to_s, 'O'}
+    broadcast(@board)
   end
 
   def end_game
@@ -31,16 +45,19 @@ class TicTacToe
   def start_game
     @whose_turn = rand(1..2)
     @message_history = []
-    @moves = [[],[],[]]
+    @moves = [[],[]]
+    @board = EMPTY_BOARD
     broadcast("Player #{@whose_turn} goes first.")
   end
 
   def take_turn(space_number)
-    if !@p1_squares.include(space_number) && !@p2_squares.include(space_number)
-      @moves[@whose_turn].append(space_number)
+    if !@moves[0].include(space_number) && !moves[1].include(space_number)
+      @moves[@whose_turn - 1].append(space_number)
+      draw_board
       broadcast("Player #{@whose_turn} moved to space #{space_number}.")
       # and then check for a win
       check_for_win
+      change_player
     else
       broadcast("Sorry Player #{@whose_turn}, that space is not available.")
     end
