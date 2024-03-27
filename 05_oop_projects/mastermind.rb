4225e768a6972dc3f7ec7275936cb1ace34c1004
Chris <christopher@christophervigliotti.com>
2024-03-27 16:29:43 -0400

    it works

--- 05_oop_projects/mastermind.rb
+++ 05_oop_projects/mastermind.rb
@@ -1,89 +1,127 @@
 # plays the game
 class Game
-    attr_accessor :game
-    def initialize
-        @game = Mastermind.new
-        play_game
-    end
+  attr_accessor :game
 
-    def play_game
-        until @game.game_over do
-            @game.take_turn(gets.chomp)
-        end        
+  def initialize
+    @game = Mastermind.new
+  end
+
+  def play_game
+    until @game.game_over 
+      @game.take_turn(gets.chomp) 
     end
+  end
 end
 
 # the game
 class Mastermind
-    HEADER = "* * * MASTERMIND * * *  "
-    COLORS = ['R','G','B']    
-    MAX_TURNS = 8
-    ANSWER_SIZE = 3
-    attr_reader :answer, :turn_number, :game_over, :guesses, :message
+  HEADER = '* * * MASTERMIND * * *'
+  COLORS = ['R','G','B','Y','P']
+  DIVIDER = '----------------------'
+  MAX_TURNS = 8
+  ANSWER_SIZE = 4
+  attr_accessor :answer, :turn_number, :game_over, :guesses, :message
 
-    def initialize
-        @guesses = []
-        reset_game
-    end
+  def initialize
+    system('clear')
+    @guesses = []
+    reset_game
+  end
 
-    def draw_board
-        system('clear')
-        puts HEADER
-        puts "Turn #{turn_number} of #{MAX_TURNS} | Colors #{COLORS} | Answer #{@answer}"
-        puts @message
-        puts @guesses
+  def draw_board
+    system('clear')
+    puts HEADER
+    puts DIVIDER
+    puts 'HISTORY'
+    @guesses.each do |n|
+      puts n.to_s
     end
+    puts DIVIDER
+    puts @message
+    puts DIVIDER
+    puts "Turn #{turn_number} of #{MAX_TURNS} | Color choices: #{COLORS}"
+    puts "Respond by typing #{ANSWER_SIZE} characters then hitting enter."
+    #puts "Answer #{@answer}"
+    puts DIVIDER
+  end
 
-    def generate_random_answer
-        @answer = []
-        ANSWER_SIZE.times do
-            answer.push(COLORS[rand(1..COLORS.size)-1])
-        end
+  def get_correct_positions(guess)
+    correct_positions = []
+    guess.each_with_index do |element,index|
+      puts index
+      correct_position = element == @answer[index] ? true : false
+      correct_positions.push(correct_position)
     end
+    correct_positions
+  end
 
-    def handle_correct_guess(guess)
-      @message = 'You win!'
-      draw_board
-      @game_over = true
+  def get_correct_colors(guess)
+    correct_colors = []
+    guess.each do |n|
+      correct_colors.append(n) if @answer.include?(n)
     end
+    correct_colors.uniq
+  end
 
-    def handle_incorrect_guess(guess)
-      @message = 'Incorrect guess.  TODO: provide feedback here'
-      draw_board
+  def generate_random_answer
+    @answer = []
+    ANSWER_SIZE.times do
+      answer.push(COLORS[rand(1..COLORS.size)-1])
     end
+  end
 
-    def handle_valid_guess(guess)
-      guess_is_correct = is_guess_correct(guess)
-      add_to_guesses(guess)
-      handle_correct_guess(guess) if guess_is_correct
-      handle_incorrect_guess(guess) if !guess_is_correct
-    end
+  def handle_correct_guess(guess)
+    @message = "Guess: '#{guess.to_s}'. You win!"
+    draw_board
+    @game_over = true
+  end
 
-    def is_guess_correct
+  def handle_incorrect_guess(guess)
+    has_another_turn = turn_number + 1 < MAX_TURNS
+    correct_positions = get_correct_positions(guess)
+    correct_colors = get_correct_colors(guess)
+    if_has_another_turn_suffix = has_another_turn ? 'Guess again.' : 'No more guesses.'
+    @message = "Turn #{@turn_number}. Guess No '#{guess.to_s}'. Correct positions '#{correct_positions}'. Correct colors '#{correct_colors}'. #{if_has_another_turn_suffix}"
+    @turn_number = @turn_number.next if has_another_turn
+    draw_board
+  end
 
-    end
+  def handle_valid_guess(guess)
+    guess_is_correct = guess_correct?(guess)
+    guesses.append(guess)
+    handle_correct_guess(guess) if guess_is_correct
+    handle_incorrect_guess(guess) unless guess_is_correct
+  end
 
-    def is_guess_valid(guess)
-      valid_guess = true
-      guess.chars.each do |n|
-        valid_guess = false if !colors.include?(n)
-      end
-      valid_guess
-    end
+  def guess_correct?(guess)
+    puts "def guess_correct?, guess = '#{guess}'"
+    puts "def guess_correct?, @answer = '#{@answer}'"
+    puts "def guess_correct?? '#{guess == @answer}'"
+    guess == @answer
+  end
 
-    def reset_game
-        @turn_number = 1
-        @game_over = false
-        generate_random_answer
-        draw_board
+  def guess_valid?(guess)
+    valid_guess = true
+    guess.each do |n|
+      valid_guess = false unless COLORS.include?(n)
     end
-    
-    def take_turn(guess)
-      guess = guess.upcase
-      guess_is_valid = is_guess_valid(guess)
-      handle_valid_guess(guess) if guess_is_valid
-      # TODO: continue here
-    end
+    valid_guess
+  end
+
+  def reset_game
+    @turn_number = 1
+    @game_over = false
+    generate_random_answer
+    draw_board
+  end
+
+  def take_turn(guess_string)
+    guess = guess_string.upcase.chars
+    guess_is_valid = guess_valid?(guess)# && guess.length == ANSWER_SIZE.size
+    handle_valid_guess(guess) if guess_is_valid
+    # TODO: continue here
+  end
 end
 
 a_game = Game.new
+a_game.play_game
